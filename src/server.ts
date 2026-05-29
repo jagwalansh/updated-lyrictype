3274af87a8b911400075ec653423f81fd164071b
Ansh Jagwal <anshjagwal02@gmail.com>
2026-05-29 23:04:43 +0530

    Improve sitemap metadata

--- src/server.ts
+++ src/server.ts
@@ -126,14 +126,15 @@ export default {
       }
 
       if (url.pathname === "/sitemap.xml") {
+        const lastmod = "2026-05-29";
         const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>https://keyverse.me/</loc><priority>1.0</priority><changefreq>daily</changefreq></url>
-  <url><loc>https://keyverse.me/recommended</loc><priority>0.8</priority><changefreq>weekly</changefreq></url>
-  <url><loc>https://keyverse.me/leaderboard</loc><priority>0.7</priority><changefreq>daily</changefreq></url>
-  <url><loc>https://keyverse.me/support</loc><priority>0.5</priority><changefreq>monthly</changefreq></url>
-  <url><loc>https://keyverse.me/terms</loc><priority>0.3</priority><changefreq>yearly</changefreq></url>
-  <url><loc>https://keyverse.me/privacy</loc><priority>0.3</priority><changefreq>yearly</changefreq></url>
+  <url><loc>https://keyverse.me/</loc><lastmod>${lastmod}</lastmod></url>
+  <url><loc>https://keyverse.me/recommended</loc><lastmod>${lastmod}</lastmod></url>
+  <url><loc>https://keyverse.me/leaderboard</loc><lastmod>${lastmod}</lastmod></url>
+  <url><loc>https://keyverse.me/support</loc><lastmod>${lastmod}</lastmod></url>
+  <url><loc>https://keyverse.me/terms</loc><lastmod>${lastmod}</lastmod></url>
+  <url><loc>https://keyverse.me/privacy</loc><lastmod>${lastmod}</lastmod></url>
 </urlset>`;
         return new Response(sitemap, {
           status: 200,
